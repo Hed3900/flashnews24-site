964473e09097f369ffc224953943bd3f9a1c2cf5
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-28 21:37:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import BreakingTicker from "./components/BreakingTicker";
-import Home from "./pages/Home";
 import CategoryBar from "./components/CategoryBar";
+import Home from "./pages/Home";
 
 export default function App() {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
   return (
     <>
       <Header />
-<BreakingTicker />
-<CategoryBar />
-<Home />
+
+      <BreakingTicker />
+
+      <CategoryBar
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+
+      <Home
+        selectedCategory={selectedCategory}
+      />
     </>
   );
 }
