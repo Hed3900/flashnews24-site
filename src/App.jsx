5f4ca5140a43d1d7d9b93d3d2f09078dc3e99533
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-28 21:23:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header";
 import BreakingTicker from "./components/BreakingTicker";
 import Home from "./pages/Home";
+import CategoryBar from "./components/CategoryBar";
 
 export default function App() {
   return (
     <>
       <Header />
-      <BreakingTicker />
-      <Home />
+<BreakingTicker />
+<CategoryBar />
+<Home />
     </>
   );
 }
