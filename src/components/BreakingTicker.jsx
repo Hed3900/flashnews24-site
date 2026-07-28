c1c4cdd7bfea06451753b4334075a63c9cd02d2d
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-28 20:40:20 +0530

    Create BreakingTicker.jsx

---
+++ src/components/BreakingTicker.jsx
@@ -0,0 +1,16 @@
+export default function BreakingTicker() {
+  return (
+    <div
+      style={{
+        background: "#d60000",
+        color: "#fff",
+        padding: "10px",
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+        fontWeight: "bold",
+      }}
+    >
+      🚨 BREAKING: Welcome to FlashNews24
+    </div>
+  );
+}
