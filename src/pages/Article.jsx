a37fd7b052d77fbbb8f025fcb17886348c32b282
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-30 10:57:02 +0530

    Update Article.jsx

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -126,18 +126,6 @@ const nextArticle =
     ? allPosts[currentIndex + 1]
     : null;
     return (
-      <div
-  style={{
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: `${progress}%`,
-    height: "4px",
-    background: "#d60000",
-    zIndex: 99999,
-    transition: "width .1s linear",
-  }}
->
   <div
     style={{
       maxWidth: "760px",
@@ -146,6 +134,17 @@ const nextArticle =
       color: "#fff",
     }}
   >
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: `${progress}%`,
+        height: "4px",
+        background: "#d60000",
+        zIndex: 99999,
+      }}
+    />
       <button
         onClick={() => navigate(-1)}
         style={{
