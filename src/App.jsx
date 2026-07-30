913d33e052a22322a1303489a349b2ff097a0217
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-30 13:24:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -42,21 +42,7 @@ export default function App() {
           vapidKey: "BNOWKXK21uLfihl_fg5BfRWkRH99kHGkZa5L-n7Oyhwj8b4FGrNRSBiV-ttSQQ3KMTbTIdLT2WU7gfvJ5O74jH0",
         });
 
-        document.body.insertAdjacentHTML(
-  "beforeend",
-  `<div style="
-    position:fixed;
-    bottom:10px;
-    left:10px;
-    right:10px;
-    background:#111;
-    color:#fff;
-    padding:10px;
-    font-size:12px;
-    z-index:99999;
-    word-break:break-all;
-  ">${token}</div>`
-);
+        alert("Notification setup started");
       } catch (err) {
         console.error("FCM Error:", err);
       }
